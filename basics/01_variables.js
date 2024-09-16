2d131ef78ab7c4d6694a1ca87a39b7b17bd10c2c
QuaziBakhtiyarAli <68594764+Quazi-Bakhtiyar@users.noreply.github.com>
2024-09-16 08:36:59 +0000

    second commit

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 121212;
+let cityName = "delhi"
+var mobileNo = 44
+stateName = "NewDelhi"
+
+let myName
+
+console.log(mobileNo)
+console.log(typeof(stateName))
+
+
+// dont use var as it is fucntional scope instead use let which is block scope.
+
+
+console.table([accountId, cityName, mobileNo, stateName, myName])
---
+++ basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // using newer version of javascript
+
+//alert(2+2) // using node js not browser so it will give error here.
+
+console.log(typeof null) //null is standalone value and its an object type.
+console.log(typeof undefined)  // undefined 
+
+// primitive data type
+/* 
+string
+number
+boolean
+bigInt
+null 
+undefined
+symbol
+
+*/
+
+// non-primitive datatypes
+
+// object
