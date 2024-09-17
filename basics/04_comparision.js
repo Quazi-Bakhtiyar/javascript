7e419df00886c733361431e47280c07b49c6b9a3
QuaziBakhtiyarAli <68594764+Quazi-Bakhtiyar@users.noreply.github.com>
2024-09-17 16:16:32 +0000

    comparision

---
+++ basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log(null == 0)
+// console.log(null > 0)
+// console.log(null>=0) // here null is first converted into number which is 0 and then compared 
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined > 0)
+// never use these type of conversion declared above 
+
+
+// === -> Strict check
+
+console.log("3"==3)
+console.log("3"===3)
