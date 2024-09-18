fff70966207db22f7cac162d8babc52bca15ecc0
QuaziBakhtiyarAli <68594764+Quazi-Bakhtiyar@users.noreply.github.com>
2024-09-18 15:53:55 +0000

    time updated properties

--- basics/07_date_time.js
+++ basics/07_date_time.js
@@ -13,4 +13,13 @@ let myCreatedDate = new Date(2023,0,12,5,4)
 console.log(myCreatedDate.toDateString())
 console.log(myCreatedDate.toLocaleString())
 
-let newDate = Date.now()
+let timeStamp  =  Date.now()
+console.log(timeStamp)
+
+console.log(myDate.toLocaleString("default",{
+    weekday : "long"
+}))
+
+console.log(myDate.getMonth())
+console.log(myDate.getDate())
+console.log(myDate.getFullYear())
