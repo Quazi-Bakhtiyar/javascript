05e6d1ccbef1b801b9ea9029e3c1134bf10915c7
QuaziBakhtiyarAli <68594764+Quazi-Bakhtiyar@users.noreply.github.com>
2024-09-18 08:19:36 +0000

    random

---
+++ basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 40
+
+const balance = new Number(100)
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(3))
+
+const number = 100000000
+//console.log(number.toLocaleString('en-IN'))
+
+// +++++++++++++++++++++++ Maths++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.PI)
+// console.log(Math.min(1,2,3))
+// console.log(Math.max(1,2,3))
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 50
+// to get the value of your desire or between any of the two numbers 
+// Math.random()*(max-min) + min use this formula
+console.log((Math.random() * (max - min)) + min)
