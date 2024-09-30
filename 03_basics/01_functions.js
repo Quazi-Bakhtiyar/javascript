1771659d3aff68af43469b2bf5f03cdf62cdf567
QuaziBakhtiyarAli <68594764+Quazi-Bakhtiyar@users.noreply.github.com>
2024-09-30 05:56:50 +0000

    funtions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,41 @@
+function addTwoNumbers(number1 , number2){
+    return number1+number2;
+} // accepting the parameters 
+
+console.log(addTwoNumbers(12,12)) // passing the arguments 
+
+function UserLoginMessage(user = "sam"){ // or give a default value
+    if(!user){
+        console.log("please enter the username")
+        return
+    }
+    return `${user} logged in.`
+}
+
+console.log(UserLoginMessage("quazi"))
+console.log(UserLoginMessage()) // undefined 
+
+// Use case of Rest parameter in javascript  ( rest parameter as well as spread operator both uses three dots ...)
+
+function calculateCart(...price){
+    return price
+}
+
+console.log(calculateCart(100,200,2000,300))
+
+// handling obejct in functions
+
+function HandleObject(anyobject){
+    console.log(`username is ${anyobject.name} and price is ${anyobject.price}`)
+}
+
+HandleObject({name:"quazi", price:3000})
+
+// handling arrays in functions
+
+myarray = [11,1,2,1,2]
+
+function handleArr(getArr){
+    return getArr[4]
+}
+console.log(handleArr(myarray))
