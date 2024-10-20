b6fc777125b7265a3cb8550d7fc95f049494680d
Quazi <indianmedievelhistory@gmail.com>
2024-10-20 18:26:48 +0530

    while and do while loop in javascript

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while loop in javascript
+let i = 1;
+while (i<=10) {
+    console.log(i)
+    i++
+}
+
+let myArray = ["flash", "Batman", "Superman"]
+
+let arr = 0
+while (arr<myArray.length) {
+    console.log(myArray[arr])
+    arr++
+}
+
+// do while loop in javascript
+
+let score = 1
+do {
+    console.log(score)
+    score++
+} while (score<=10);
