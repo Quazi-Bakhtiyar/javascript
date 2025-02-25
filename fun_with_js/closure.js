58e7f20346d0a1dff2c77b3a3bb121102da312bd
Quazi Bakhtiyar Ali <indianmedievelhistory@gmail.com>
2025-02-25 21:54:34 +0530

    closure property

--- fun_with_js/closure.js
+++ fun_with_js/closure.js
@@ -1,12 +1,9 @@
-function makeAdder(x) {
-    return function (y) {
-      return x + y;
-    };
+function init() {
+  var name = "Mozilla"; // name is a local variable created by init
+  function displayName() {
+    // displayName() is the inner function, that forms a closure
+    console.log(name); // use variable declared in the parent function
   }
-  
-  const add5 = makeAdder(5);
-  const add10 = makeAdder(10);
-  
-  console.log(add5(2)); // 7
-  console.log(add10(2)); // 12
-  
+  displayName();
+}
+init();
